2bd94a5c79323f290d5c6aacd012d87db32d4092
gracedward <edward.chung.designer@gmail.com>
2022-06-02 12:09:52 +0100

    finished Part2.1

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,55 @@
+const Course = ({ course }) => {
+  const {id,name,parts} = course;
+  return (
+    <>
+      <Header name={name} />
+      <Content parts={parts} />
+    </>
+  )
+};
+
+const Header = ({ name }) => <h1>{name}</h1>;
+
+const Total = ({ sum }) => <p>Number of exercises {sum}</p>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <>
+    {
+      parts.map(part => <Part key={part.id} part={part} />)
+    }  
+  </>
+);
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+        id: 1,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+        id: 2,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+        id: 3,
+      },
+    ],
+  };
+
+  return <Course course={course} />;
+};
+
+export default App;
