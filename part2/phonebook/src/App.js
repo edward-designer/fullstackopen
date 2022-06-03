2d25002951ba64a162746b5e817a3b849d01f6b0
gracedward <edward.chung.designer@gmail.com>
2022-06-04 00:25:02 +0100

    finished part2.20

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -17,17 +17,25 @@ const PersonsForm = ({onSubmitHandler,onNameChangeHandler,onNumberChangeHandler,
       </form>
 )      
 
-const Persons = ({persons,searchText, deleteHandler}) => {
+const Persons = ({persons,searchText, deleteHandler, setMessage}) => {
   const regex = new RegExp(searchText, 'i'); // for regex
-  return (persons.filter(person=>person.name.match(regex)).map(person=><li key={person.name}>{person.name} {person.number} <button type="button" onClick={deleteHandler(person)}>delete</button></li>))
+  return (persons.filter(person=>person.name.match(regex)).map(person=><li key={person.name}>{person.name} {person.number} <button type="button" onClick={deleteHandler(person,setMessage)}>delete</button></li>))
 }
 
+const Notification = ({message, setMessage}) => {
+  if(Object.keys(message).length === 0) return;
+  setTimeout(()=>setMessage({}),5000)
+  let msgStyle={marginBottom:'10px',border:'1px solid green',backgroundColor:'#feffeb',color:'darkgreen',padding:'10px'}
+  if(message.error){msgStyle={marginBottom:'10px',border:'1px solid red',backgroundColor:'#feffeb',color:'darkred',padding:'10px'}}
+  return <div style={msgStyle}>{message[Object.keys(message)[0]]}</div>;
+}
 const App = () => {
   const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [searchText, setSearchText] = useState('')
-
+  const [message, setMessage] = useState({})
+  
   useEffect(()=>{
     const promise = serverComm.getAll();
     promise.then(data=>{setPersons(data)})
@@ -43,30 +51,35 @@ const App = () => {
       if(window.confirm(`${newName} is already added to phonebook, replace with the new number ${newNumber}?`)){
           const personID = persons.find(person=>person.name===newName&&person.phone!==newNumber).id;
           const updatedPerson = {name: newName, number: newNumber, id: personID};
-          serverComm.update(personID,updatedPerson).then(response=>{setPersons(persons.filter(persons => persons.id !== personID).concat(updatedPerson))})
+          serverComm.update(personID,updatedPerson).then(response=>{setPersons(persons.filter(persons => persons.id !== personID).concat(updatedPerson));setNewName('');setNewNumber('');setMessage({success:`Updated the phone number for ${newName}`})})
+          .catch(err => console.error(err))
       }
     }else{
-      serverComm.create({name:newName, number:newNumber}).then(response=>{setNewName('');setNewNumber('');setPersons(persons.concat(response))})
+      serverComm.create({name:newName, number:newNumber}).then(response=>{setNewName('');setNewNumber('');setPersons(persons.concat(response));setMessage({succeess:`Added ${newName}`});}).catch(err => console.error(err))
     }
   }
   const seachChangeHandler = (e) => {
     setSearchText(e.target.value);
   }
-  const deleteHandler = ({id,name}) => (e) => {
+  const deleteHandler = (person,setMessge) => (e) => {
+    const {id,name} = person;
     if(window.confirm(`Delete ${name}?`)){
-      serverComm.deleteEntry({id}).then(response => {setPersons(persons.filter(person=>person.id!==id))})
+      serverComm.deleteEntry({id}).then(response => {setPersons(persons.filter(person=>person.id!==id))}).catch(err =>{setMessge({error:`Information of ${name} has already been removed from the server.`});setPersons(persons.filter(person=>person.id!==id));console.error(err)})
     }
   }
   const personsFormProps = {newName, newNumber,onSubmitHandler,onNameChangeHandler,onNumberChangeHandler}
 
+  console.log(message);
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message={message} setMessage={setMessage} />
       <Filter searchText={searchText} seachChangeHandler={seachChangeHandler} />
       <h2>Add a New Contact</h2>
       <PersonsForm {...personsFormProps}/>
       <h2>Numbers</h2>
-      <Persons searchText={searchText} persons={persons} deleteHandler={deleteHandler}/>
+      <Persons searchText={searchText} persons={persons} setMessage={setMessage} deleteHandler={deleteHandler}/>
     </div>
   )
 }
