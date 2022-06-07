26f13577e1e5732926e94368483959ce0511cef8
gracedward <edward.chung.designer@gmail.com>
2022-06-07 19:49:42 +0100

    updates

--- part2/phonebook/src/server.js
+++ part2/phonebook/src/server.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'https://sleepy-harbor-23606.herokuapp.com/api/persons'
 
 const create = newObj => {
     const request = axios.post(baseUrl,newObj)
